570f48849cecc0810b6bc53f12b80a37bc205b59
mudangler <mud.angler1220@gmail.com>
2024-10-31 10:32:39 +0900

    テスト

--- patterns/text-feature-grid-3-col.php
+++ patterns/text-feature-grid-3-col.php
@@ -21,7 +21,7 @@
 		<!-- /wp:spacer -->
 
 		<!-- wp:paragraph {"align":"center"} -->
-		<p class="has-text-align-center"><?php echo esc_html_x( 'テストテストテストテストテストテスト', 'Sub-heading of the features', 'twentytwentyfour' ); ?></p>
+		<p class="has-text-align-center"><?php echo esc_html_x( 'テストテストテストテストテストテストテストテストテストテストテストテステストテストテストテストテストテス', 'Sub-heading of the features', 'twentytwentyfour' ); ?></p>
 		<!-- /wp:paragraph -->
 	</div>
 	<!-- /wp:group -->
